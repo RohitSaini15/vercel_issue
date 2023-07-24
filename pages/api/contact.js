98e4c44a5eeaddaafb4a123c333f9259bfa09b56
rohit saini <r7665415553@gmail.com>
2023-07-24 14:59:50 +0530

    uncomment doc.useService  minimal functionality in contact api

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -3,10 +3,10 @@ import { GoogleSpreadsheet } from 'google-spreadsheet';
 
 const doc = new GoogleSpreadsheet(process.env.SPREAD_SHEET_ID);
 
-// doc.useServiceAccountAuth({
-//     client_email: process.env.CLIENT_EMAIL,
-//     private_key: process.env.PRIVATE_KEY,
-// });
+doc.useServiceAccountAuth({
+    client_email: process.env.CLIENT_EMAIL,
+    private_key: process.env.PRIVATE_KEY,
+});
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
