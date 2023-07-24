5e76878560f50c64587f967cb172c8b3f985526e
rohit saini <r7665415553@gmail.com>
2023-07-24 14:54:57 +0530

    provide minimal functionality in contact api

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -10,24 +10,25 @@ doc.useServiceAccountAuth({
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-        try {
-            // Extract the form data from the request body
-            const { name, email, message } = req.body;
+        res.status(200).json({message: "success"})
+        // try {
+        //     // Extract the form data from the request body
+        //     const { name, email, message } = req.body;
 
-            // Load the Google Sheets document
-            await doc.loadInfo();
+        //     // Load the Google Sheets document
+        //     await doc.loadInfo();
 
-            // Select the first sheet 
-            const sheet = doc.sheetsByIndex[0];
+        //     // Select the first sheet 
+        //     const sheet = doc.sheetsByIndex[0];
 
-            // Append the form data as a new row in the sheet
-            await sheet.addRow({ Name: name, Email: email, Message: message });
+        //     // Append the form data as a new row in the sheet
+        //     await sheet.addRow({ Name: name, Email: email, Message: message });
 
-            res.status(200).json({ message: 'success' });
-        } catch (error) {
-            console.error('Error:', error);
-            res.status(500).json({ message: 'An error occurred' });
-        }
+        //     res.status(200).json({ message: 'success' });
+        // } catch (error) {
+        //     console.error('Error:', error);
+        //     res.status(500).json({ message: 'An error occurred' });
+        // }
     } else {
         res.status(405).json({ message: 'Method Not Allowed' });
     }
